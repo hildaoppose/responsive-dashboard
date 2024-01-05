718f28a1fb0a1e607a8d97afcbeea47faa6e68a5
hildaoppose <hildaopposekazungu@gmail.com>
2024-01-06 00:45:29 +0300

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Silk Corset Dress',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Luxury Silk Fur Pajamas',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Luxury Candle Set',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
